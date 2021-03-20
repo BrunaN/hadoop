3eca221b9bb816bfc9818ef56ba0147bbed657b2
Bruna Neves <brubsnev@gmail.com>
2021-03-20 17:50:32 -0300

    classe comparaora

--- Hadoop/src/main/java/br/ufc/br/Comparator.java
+++ Hadoop/src/main/java/br/ufc/br/Comparator.java
@@ -2,6 +2,9 @@ package br.ufc.br;
 
 import java.io.IOException;
 
+import java.util.List;
+import java.util.Arrays;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -12,41 +15,31 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-import com.google.gson.Gson;
-
-import br.ufc.br.model.Tweet;
-
-public class ItemC {
+public class Comparator {
 
 	public static class ReviewsMapper extends Mapper<Object, Text, IntWritable, Text> {
 
 		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 
-			Gson gson = new Gson();
-			Tweet t = gson.fromJson(value.toString(), Tweet.class);
+			String text = value.toString();
+			List<String> tokens = Arrays.asList(text.split("\t"));
+			String counter = tokens.get(1);
 
-			String reviews = t.getAuthor().getReviews().replace(",", ".");
-			String id = t.getId();
-			
-			if(!reviews.isEmpty()) {
-				Double numParsed = Double.parseDouble(reviews);
-				Integer reviewsNumber = (int) Math.round(numParsed);
-				context.write(new IntWritable(reviewsNumber), new Text(id));
-			}
-						
+			context.write(new IntWritable(Integer.parseInt(counter)), new Text(tokens.get(0)));
 		}
 	}
 
 	public static class DescendingIntWritableComparable extends IntWritable {
-	    /** A decreasing Comparator optimized for IntWritable. */ 
-	    public static class DecreasingComparator extends Comparator {
-	        public int compare(WritableComparable a, WritableComparable b) {
-	            return -super.compare(a, b);
-	        }
-	        public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
-	            return -super.compare(b1, s1, l1, b2, s2, l2);
-	        }
-	    }
+		/** A decreasing Comparator optimized for IntWritable. */
+		public static class DecreasingComparator extends Comparator {
+			public int compare(WritableComparable a, WritableComparable b) {
+				return -super.compare(a, b);
+			}
+
+			public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
+				return -super.compare(b1, s1, l1, b2, s2, l2);
+			}
+		}
 	}
 
 	public static void main(String[] args) throws Exception {
